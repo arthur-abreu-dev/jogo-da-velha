0cd474f79f634fb66542a06b5d945a96b80447a5
Arthur <arthur.ab@hotmail.com>
2022-03-01 20:45:36 -0300

    Primeira parte do desenvolvimento do jogo da velha

--- src/index.js
+++ src/index.js
@@ -1,17 +1,105 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Jogo extends React.Component {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    constructor(props) {
+        super(props);
+        this.state = {
+            quadrados: Array(9).fill(null),
+            proximoX: true,
+        };
+    }
+
+    renderQuadrado(i) {
+        return (<Quadrado value={this.state.quadrados[i]}
+            onClick={() => { this.handleClick(i) }} />);
+    }
+
+    handleClick(i) {
+        const quadrados = this.state.quadrados.slice();
+        const winner = calculateWinner(this.state.quadrados);
+        if (quadrados[i] == null && !winner) {
+            quadrados[i] = (this.state.proximoX ? 'X' : 'O');
+            this.setState({
+                quadrados: quadrados,
+                proximoX: !this.state.proximoX,
+            });
+        }
+    }
+
+    render() {
+        const winner = calculateWinner(this.state.quadrados);
+        let status;
+        if (winner) {
+            status = "Ganhador: " + winner;
+        } else {
+            status = "Próximo jogador: " + (this.state.proximoX ? 'X' : 'O');
+        }
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="coluna-jogo">
+                    {this.renderQuadrado(0)}
+                    {this.renderQuadrado(1)}
+                    {this.renderQuadrado(2)}
+                </div>
+                <div className="coluna-jogo">
+                    {this.renderQuadrado(3)}
+                    {this.renderQuadrado(4)}
+                    {this.renderQuadrado(5)}
+                </div>
+                <div className="coluna-jogo">
+                    {this.renderQuadrado(6)}
+                    {this.renderQuadrado(7)}
+                    {this.renderQuadrado(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+
+function Quadrado(props) {
+    return (
+        <button className="quadrado" onClick={() => { props.onClick() }}>
+            {props.value}
+        </button>
+    );
+}
+
+class JogoDaVelha extends React.Component {
+    render() {
+        return <div className="jogao">
+            <div className="jogao-brabo">
+                <div><Jogo /></div>
+            </div>
+        </div>;
+    }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+// ======================
+
+ReactDOM.render(<JogoDaVelha />,
+    document.getElementById('root'));
